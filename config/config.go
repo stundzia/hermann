365d88a86ab1e35b10e0204bb3620172a8068eb8
Paulius Stundzia <paulius.stundzia@oxylabs.io>
2021-05-12 09:36:55 +0300

    Initial commit

---
+++ config/config.go
@@ -0,0 +1,45 @@
+package config
+
+import (
+	"flag"
+	"github.com/spf13/viper"
+	"go.uber.org/zap"
+	"sync"
+)
+
+type Config struct {
+	Kafka *kafka
+}
+
+type kafka struct {
+	Address string
+	Topic string
+	Topics []string
+}
+
+
+var configPath = flag.String("config", "conf", "path to the config")
+var configInstance *Config
+var once sync.Once
+
+func GetConfig() *Config {
+	once.Do(func() {
+		setConfig(zap.NewNop())
+	})
+	return configInstance
+}
+
+func setConfig(logger *zap.Logger) {
+	config := viper.New()
+	config.SetConfigName("base")
+	config.AddConfigPath(*configPath)
+	err := config.ReadInConfig() // Find and read the config file
+	if err != nil {              // Handle errors reading the config file
+		logger.Fatal("couldn't read config file", zap.Error(err))
+	}
+
+	err = config.Unmarshal(&configInstance)
+	if err != nil {
+		panic(err)
+	}
+}
---
+++ kafka/const.go
@@ -0,0 +1,4 @@
+package kafka
+
+
+const DefaultConsumerGroup = "hermann"
